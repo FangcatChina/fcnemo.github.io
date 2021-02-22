d0067276be4c5f9da6445fc0914e48fd0af39cb3
Fangcat <2950966704@qq.com>
2021-02-22 15:50:01 +0800

    add blog/articles/pypg.py.

---
+++ blog/articles/pypg.py
@@ -0,0 +1,19 @@
+import setuptools
+with open("README.md", "r",encoding='utf-8') as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="库名",
+    version="版本号",
+    author="作者",
+    author_email="电子邮箱",
+    description="简介",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="项目网址",
+    packages=setuptools.find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3", #Python版本（2或3）
+    "License :: OSI Approved :: MIT License", #开源许可
+    "Operating System :: OS Independent",
+    ],
+)
