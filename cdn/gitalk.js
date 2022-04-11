04f995e11295c9b1f4e1ece7bfc10d7396178ccb
Fangcat <65712297+FangcatChina@users.noreply.github.com>
2022-04-11 10:19:05 +0800

    Update and rename gitalk.html to gitalk.js

--- cdn/gitalk.js
+++ cdn/gitalk.js
@@ -1,17 +1,8 @@
-<div id="gitalk-container"></div>
 
-<script>
-
-  var gitalk = new Gitalk({
     clientID: '21d7fb822b4454e44d82',
     clientSecret: '91c0a72f04b7fa82c597d9aa098a40e26b6f1c6e',
     repo: 'Comment',
     owner: 'FangcatChina',
     admin: ['FangcatChina'],
-    id: (location.pathname).substring(0, 49),      // Ensure uniqueness and length less than 50
     distractionFreeMode: false  // Facebook-like distraction free mode
-  })
 
-  gitalk.render('gitalk-container')
-
-</script>
