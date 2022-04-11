e2dfb3cae1db540b3ece5dafd181c27f02fab0b6
Fangcat <65712297+FangcatChina@users.noreply.github.com>
2022-04-11 10:23:21 +0800

    Update gitalk.js

--- cdn/gitalk.js
+++ cdn/gitalk.js
@@ -1,8 +1,16 @@
+<div id="gitalk-container"></div>
 
+<script>
+
+  var gitalk = new Gitalk({
     clientID: '21d7fb822b4454e44d82',
     clientSecret: '91c0a72f04b7fa82c597d9aa098a40e26b6f1c6e',
     repo: 'Comment',
     owner: 'FangcatChina',
     admin: ['FangcatChina'],
     distractionFreeMode: false  // Facebook-like distraction free mode
+  })
 
+  gitalk.render('gitalk-container')
+
+</script>
